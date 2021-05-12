daa8564b2b100351f59327cadb1e6ee6f60fd3c7
Abrams27 <abramowiczmarcin99@gmail.com>
2021-05-12 09:32:12 +0200

    Exceptions show

--- src/Typechecker/Exceptions.hs
+++ src/Typechecker/Exceptions.hs
@@ -13,5 +13,26 @@ data TypecheckerException' a
   | FunctionArgumentsNameDuplicationException a [Arg]
   | InvalidReturnTypeException a RawType RawType
 
--- instance Show TypecheckerException where
---   show
+
+instance (Show a) => Show (TypecheckerException' a) where
+
+  show (UndefinedSymbolException position name) = concat [
+    "ERROR: Undefined symbol: ", show name, ", at ", show position
+    ]
+
+  show (InvalidTypeException position expectedType actualType) = concat [
+    "ERROR: Invalid type! Expected: ", show expectedType, ", got: ", show actualType, ", at ", show position
+   ]
+
+  show (InvalidFunctionArgumentsTypesException position expectedTypes actualTypes) = concat [
+    "ERROR: Invalid function arguments! Expected: ", show expectedTypes, ", got: ", show actualTypes, ", at ", show position
+   ]
+
+  show (FunctionArgumentsNameDuplicationException position arguments) = concat [
+    "ERROR: Function arguments names duplication: ", show arguments, ", at ", show position
+   ]
+
+  show (InvalidReturnTypeException position expectedType actualType) = concat [
+    "ERROR: Invalid function return type! Expected: ", show expectedType, ", got: ", show actualType, ", at ", show position
+   ]
+
--- src/Typechecker/Types.hs
+++ src/Typechecker/Types.hs
@@ -13,6 +13,21 @@ data RawType
   deriving (Eq)
 
 
+instance Show RawType where
+
+  show RTInt = "Int"
+
+  show RTString = "String"
+
+  show RTBool = "Bool"
+
+  show RTVoid = "Void"
+
+  show (RTFun argsTypes returnType) = concat [
+    "(", show argsTypes, ")", " -> ", show returnType
+    ]
+
+
 fromType :: Type -> RawType
 fromType (TInt _) = RTInt
 fromType (TString _) = RTString
